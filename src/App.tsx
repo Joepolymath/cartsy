060b5ef6486f5e7380cd7477da11508d679df4f6
Joepolymath <joshuaajagbe96@gmail.com>
2022-08-05 22:57:09 +0100

    initial commit with react-query setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+// @ts-ignore
+import { useQuery } from "react-query";
 
-function App() {
+import Drawer from "@mui/material/Drawer";
+import { LinearProgress, Grid, Badge } from "@mui/material";
+import { AddShoppingCart } from "@mui/icons-material";
+
+// styles
+import { Wrapper } from "./App.styles";
+
+// Types
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+};
+
+const getProducts = async (): Promise<CartItemType[]> => {
+  return await (await fetch("https://fakestoreapi.com/products")).json();
+};
+
+const App = () => {
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    "products",
+    getProducts
+  );
+  console.log(error);
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +51,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
